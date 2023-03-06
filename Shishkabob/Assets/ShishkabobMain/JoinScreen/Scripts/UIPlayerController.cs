e1b4f8b90f74e59c0210f5f0d8a76a4b0d7660db
tantiem <63617690+tantiem@users.noreply.github.com>
2023-03-05 19:34:16 -0700

    Made join screen more functional

--- Shishkabob/Assets/ShishkabobMain/JoinScreen/Scripts/UIPlayerController.cs
+++ Shishkabob/Assets/ShishkabobMain/JoinScreen/Scripts/UIPlayerController.cs
@@ -6,8 +6,22 @@ public class UIPlayerController : MonoBehaviour
 {
     public bool leader;
 
+    public enum CurrentUIState {PlayerJoin,MapChoice}
+    public CurrentUIState currentUIState;
+    public UIStateManager uiManager;
+
+    private void Awake() {
+        currentUIState = CurrentUIState.PlayerJoin;
+        uiManager = GameObject.Find("Canvas").GetComponent<UIStateManager>();
+    }
     public void Submit(InputAction.CallbackContext context)
     {
-        
+        if(leader)
+        {
+            if(currentUIState == CurrentUIState.PlayerJoin)
+            {
+                uiManager.SwitchPositions();
+            }
+        }
     }
 }
---
+++ Shishkabob/Assets/ShishkabobMain/JoinScreen/Scripts/UIStateManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIStateManager : MonoBehaviour
+{
+    public RectTransform joinSection,mapSection;
+
+    public void SwitchPositions()
+    {
+        Vector2 wantedJoinPos = mapSection.transform.localPosition;
+        Vector2 wantedMapPos = joinSection.transform.localPosition;
+
+        StartCoroutine(LerpSwap(2f,new Vector2(5000,0),new Vector2(0,0))); //busted right now, but it works
+    }
+
+    IEnumerator LerpSwap(float t,Vector2 wantedJoinPos, Vector2 wantedMapPos)
+    {
+        float time = 0f;
+        Vector2 curJoinPos = joinSection.transform.localPosition;
+        Vector2 curMapPos = mapSection.transform.localPosition;
+        while(time < t)
+        {
+            yield return new WaitForEndOfFrame();
+            time += Time.deltaTime;
+
+            joinSection.transform.localPosition = Vector2.Lerp(curJoinPos,wantedJoinPos,time);
+            mapSection.transform.localPosition = Vector2.Lerp(curMapPos,wantedMapPos,time);
+        }
+        joinSection.transform.localPosition =  wantedJoinPos;
+        mapSection.transform.localPosition = wantedMapPos;
+
+    }
+}
